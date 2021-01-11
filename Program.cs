be57ad6175eb2508f0dd5aafc2cc12a6f49b3fbb
Yash Tiwari <yasht.2701@gmail.com>
2021-01-11 20:40:19 +0530

    Add helloworld.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+            System.Console.WriteLine("MLH <3");
+        }
+    }
+}
